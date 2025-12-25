dbb2fc84345f26bb61f1e89fc71bb13b1dfc5ebf
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-25 22:19:24 +0800

    feat/kam..sh

--- kam.sh
+++ kam.sh
@@ -2,7 +2,7 @@
 # kam install https://github.com/LIghtJUNction/MagicNet.git
 set -euo pipefail
 
-command -v kam >/dev/null || { pkg install -y rust && pkg install -y openssl && cargo install kam }
+command -v kam >/dev/null || { pkg install -y rust && pkg install -y openssl && cargo install kam; }
 
 command -v gh  >/dev/null || pkg install -y github-cli
 
