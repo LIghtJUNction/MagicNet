0baf48b0fe766b4ed4080f24f25a946900235526
lightjunction <lightjunction.me@gmail.com>
2025-12-27 16:54:33 +0800

    sync

--- src/MagicNet/customize.sh
+++ src/MagicNet/customize.sh
@@ -16,6 +16,7 @@
 # 依赖控制,文件共享
 # 自动提取模块.local到kam共享目录，采用硬链接方式
 # 为保证模块卸载时候不会有残留文件，uninstall.sh文件不要删
+import __customize__
 
 import i18n
 import lang
--- src/MagicNet/uninstall.sh
+++ src/MagicNet/uninstall.sh
@@ -2,6 +2,5 @@
 
 [ -f "${MODPATH}/lib/kamfw/.kamfwrc" ] && . "$MODPATH/lib/kamfw/.kamfwrc" || abort '! File ".kamfwrc" does not exist!'
 # 作者注：
-# 啥也不干上面这行也不要删，用来自动清理残留文件
-# 清理的是安装时自动提取到/data/adb/kam/bin的部分，其他的不管
-#
+# 用来自动清理残留文件
+import __uninstall__
