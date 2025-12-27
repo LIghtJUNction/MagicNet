21ab5dc14f36537274ba974249d2d007c0d64a22
lightjunction <lightjunction.me@gmail.com>
2025-12-28 04:59:44 +0800

    sync

--- src/MagicNet/uninstall.sh
+++ src/MagicNet/uninstall.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=ash
 
-[ -f "${MODPATH}/lib/kamfw/.kamfwrc" ] && . "$MODPATH/lib/kamfw/.kamfwrc" || abort '! File ".kamfwrc" does not exist!'
+MODDIR=${0%/*}
+[ -f "${MODDIR}/lib/kamfw/.kamfwrc" ] && . "$MODDIR/lib/kamfw/.kamfwrc" || abort '! File ".kamfwrc" does not exist!'
 # 作者注：
 # 用来自动清理残留文件
 import __uninstall__
