07a2d3c7eb30d6216fa0293b8e4fa14b3c2b1767
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-29 06:56:52 +0800

    重大更新，多订阅支持+yawl锚点减少大量重复行+精简代理组+改进dns配置

--- hooks/pre-build/6000.check_config.sh
+++ hooks/pre-build/6000.check_config.sh
@@ -2,13 +2,16 @@
 
 . "$KAM_HOOKS_ROOT/lib/utils.sh"
 
-
 export HOME=$KAM_MODULE_ROOT
 
 if is_ci; then
-  exit 0
+    URL=$(curl -s api.github.com | grep "browser_download_url.*linux-amd64-v1.*gz" | cut -d '"' -f 4 | head -n 1)
+    curl -L -o mihomo.gz "$URL"
+    gunzip mihomo.gz
+    chmod +x mihomo
+    sudo mv mihomo /usr/local/bin/mihomo
 fi
 
 require_command mihomo "arch: paru -S mihomo"
 
-mihomo
+mihomo -t $HOME/.config/mihomo/config.yaml
