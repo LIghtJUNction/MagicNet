e0a6b91e600be45a5bc1d2c77acb953b9cdc2f9b
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-28 16:19:35 +0800

    Update kam.sh

--- kam.sh
+++ kam.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 command -v kam >/dev/null || { pkg install -y rust && pkg install -y openssl && cargo install kam; }
 
-command -v gh  >/dev/null || pkg install -y github-cli
+command -v gh  >/dev/null || pkg install -y gh
 
 command -v cz  >/dev/null || pkg install -y python-commitizen
 
