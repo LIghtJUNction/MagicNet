2fdff1a6414f76106c26d4cca4cf5e6b699659cd
LIghtJUNction <lightjunction.me@gmail.com>
2025-12-29 21:29:39 +0800

    auto/sync

--- kam.sh
+++ kam.sh
@@ -2,7 +2,7 @@
 # kam install LIghtJUNction/MagicNet
 set -euo pipefail
 
-command -v kam >/dev/null || { pkg install -y rust && pkg install -y openssl && cargo install kam; }
+command -v kam >/dev/null || { pkg install -y rust && pkg install -y openssl perl make cmake && cargo install kam; }
 
 command -v gh  >/dev/null || pkg install -y gh
 
