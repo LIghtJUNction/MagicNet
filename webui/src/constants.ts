2a0773bed4f700772a3d0379776016421a33bd9c
LIghtJUNction <lightjunction.me@gmail.com>
2026-07-20 16:22:55 +0800

    feat(webui): add promotion URL

--- webui/src/constants.ts
+++ webui/src/constants.ts
@@ -3,6 +3,7 @@ export const CLI = `${MODULE_DIR}/cli`;
 export const SING_BOX_UI = "http://127.0.0.1:9090/ui/";
 export const REPO = "https://github.com/LIghtJUNction/MagicNet";
 export const AUTHOR_WHISPER_URL = "https://lightjunction.github.io/lightjunction/";
+export const PROMOTION_URL = "https://bizbot.zvo.cn/";
 
 export const OUTPUT_RENDER_LIMIT = 6000;
 export const CLI_TIMEOUT_MS = 45000;
