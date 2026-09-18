5527384093ff1278320a708985a60263c240995e
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-18 18:50:41 +0800

    fix(config): pin MagicSingBox DNS compatibility update

--- webui/src/components/pages/ConfigPage.vue
+++ webui/src/components/pages/ConfigPage.vue
@@ -18,8 +18,8 @@ import { MAX_LOCAL_CONFIG_BYTES, parseLocalConfigFile } from "./configFileImport
 const { state, loadConfig, saveConfig, syncConfigTemplate, loadConfigRepository, saveConfigRepository, openExternal, REPO } = useMagicNet();
 const { isRunning, withAction } = useActionLock();
 const DEFAULT_CONFIG_REPO_URL = "https://github.com/LIghtJUNction/MagicSingBox.git";
-const DEFAULT_CONFIG_REPO_REF = "ef82670ef9dc687eb1360fcde88405b0b4cd335a";
-const DEFAULT_CONFIG_REPO_SHA256 = "35d54f908ccc9cc615fabf06b1037bbfc1f6d1d9712c86ba6c9b5252cc98ae03";
+const DEFAULT_CONFIG_REPO_REF = "fae7d46df531994b4ab8bb8c3b7531a9f7b459ff";
+const DEFAULT_CONFIG_REPO_SHA256 = "dad272466757b6ef349c1ff38bc68871d2e1db50d45cd5cbf099bd60939d2506";
 const pendingConfigAction = ref<PendingToolAction | null>(null);
 const configFileInput = ref<HTMLInputElement | null>(null);
 const localJsonStatus = ref("");
